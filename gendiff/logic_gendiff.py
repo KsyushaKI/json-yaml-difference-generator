1a1d00eb7b9605305a8076bea17c5f03c33730f0
KsyushaKI <oksanaakadem39@gmail.com>
2023-02-27 21:05:37 +0300

    step2: add "-f" option to the gendiff/logic_gendiff.py

--- gendiff/logic_gendiff.py
+++ gendiff/logic_gendiff.py
@@ -3,9 +3,12 @@ import argparse
 
 parser = argparse.ArgumentParser(
     prog='gendiff',
-    description='Compares two configuration files and shows a difference.'
-    )
+    description='Compares two configuration files and shows a difference.',
+)
+
 parser.add_argument('first_file')
 parser.add_argument('second_file')
-
-args = parser.parse_args()
+parser.add_argument(
+    '-f', '--format',
+    help='set format of output',
+)
